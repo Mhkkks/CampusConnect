80d2caac8d6e46acd18355c68b3296306e3aaa2a
Ajay <Ajay>
2024-05-27 12:54:50 +0100

    formatted code

--- src/main/java/ai/verse/scheduler/Sched.java
+++ src/main/java/ai/verse/scheduler/Sched.java
@@ -16,9 +16,6 @@ import java.util.logging.Logger;
 
 @Component
 public class Sched {
-
-
-
     @Autowired
     PostRepository postRepository;
 
@@ -26,44 +23,23 @@ public class Sched {
     public void scheduledMethod() {
         System.out.println(" --------------- scheduledMethod Called on:" + new Date());
         List<PostEntity> list = postRepository.findAll();  // Get all posts from database
-      //  System.out.println(list);
 
 
-        //////////////////////////// This is for first post for demo ////////////////////////////////////////////
-        // This is for first row in Sentiment
-//        PostEntity onePost = list.get(0); // This is will get the first post (complete row of database)
-//        Sentiment sm = new Sentiment();   // This creates instance of a class (OOPS concept)  so that you can use it here
-//        String sentiment = sm.getSentimentOfText(onePost.getPost());
-      //  System.out.println(sentiment);
-        ///////////////////////// this is for ///////////////////////////////////////////////
-
-
-//        PostEntity neww = new PostEntity();
-//        neww.setPost("This is from java");
-//        postRepository.save(neww);
-
-      // Here we are iterating over all the posts and calling sentimen class
-        for(int h=0; h<list.size(); h++ )
-        {
+        // Here we are iterating over all the posts and calling sentimen class
+        for (int h = 0; h < list.size(); h++) {
 
             Sentiment sm = new Sentiment();
             PostEntity post = list.get(h);   // This will get post in a loop
             String sentm = sm.getSentimentOfText(post.getPost());
 
-           post.setSentiment(sentm);
-              postRepository.save(post);  // save in database
-
-
+            post.setSentiment(sentm);
+            postRepository.save(post);  // save in database
             System.out.println(post);
-            System.out.println(sentm);
+
         }
 
 
-
     }
-
-
-
 
 
 }
