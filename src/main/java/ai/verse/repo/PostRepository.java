f157132283415777ed519d65d3f7f1ab7b6163e5
Ajay <Ajay>
2024-05-27 13:09:16 +0100

    ADDED list with no sentiments

--- src/main/java/ai/verse/repo/PostRepository.java
+++ src/main/java/ai/verse/repo/PostRepository.java
@@ -4,11 +4,21 @@ package ai.verse.repo;
 
 import ai.verse.repo.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 // Spring Data JPA creates CRUD implementation at runtime automatically.
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
+
+
+    // Get rows where sentiment is null. NULL is different from blank
+    @Query("SELECT u FROM PostEntity u WHERE u.sentiment is null")  // jpa ql
+    List<PostEntity> findRowsWithNoSentiment();
+
+  //mysql   select * from posts where sentiment is null;
 
 
 }
--- src/main/java/ai/verse/scheduler/Sched.java
+++ src/main/java/ai/verse/scheduler/Sched.java
@@ -22,23 +22,22 @@ public class Sched {
     @Scheduled(fixedRate = 10000)
     public void scheduledMethod() {
         System.out.println(" --------------- scheduledMethod Called on:" + new Date());
-        List<PostEntity> list = postRepository.findAll();  // Get all posts from database
 
+        // List<PostEntity> list = postRepository.findAll();  // Get all posts from database
+
+        List<PostEntity> list = postRepository.findRowsWithNoSentiment();  // Lists posts with no sentiment
+
+        System.out.println("--- NUMBER OF ROWS WITH NO SENTIMENT IS:  --->>" + list.size());
 
         // Here we are iterating over all the posts and calling sentimen class
         for (int h = 0; h < list.size(); h++) {
-
             Sentiment sm = new Sentiment();
             PostEntity post = list.get(h);   // This will get post in a loop
             String sentm = sm.getSentimentOfText(post.getPost());
-
             post.setSentiment(sentm);
             postRepository.save(post);  // save in database
             System.out.println(post);
-
         }
-
-
     }
 
 
