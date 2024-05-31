836e34d326f6e02403c8d369b44cc7b3efc6063c
Ajay <Ajay>
2024-05-31 06:23:07 +0100

    API cleint and Sched file

--- src/main/java/ai/verse/ApiClient.java
+++ src/main/java/ai/verse/ApiClient.java
@@ -6,36 +6,110 @@ import java.net.URLEncoder;
 
 public class ApiClient {
 
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
 
-        ApiClient api  = new ApiClient();
+        ApiClient api = new ApiClient();
 
         api.callFacebookAPI();
     }
 
 
-    private  void callFacebookAPI()
-    {
-        String accessToken = "EAA4uBIZCLHrEBO2dn62DJh2GBvj0e8o2AWZBHbDYwkjP0YEvb1ceVoFNiGmV7tMbEAnddSHwFN52hlDVS5Ng6pKiZBZCLkNRT1H2uIaeix2RwSUnMKVQv3HKQSFYiqiQIh6BzCzMooyJvohKpETEoEOZBZALgT4Xs0kKskld1OrtnfVi5DDWytaI1T8trZCh0409Gv53VN7W6WRSVEzCQV4YfjG5uDZA5VAn";
-         String uri = "https://graph.facebook.com/v19.0/me/posts?fields=comments&access_token="+accessToken;
+    // Using an api : api client
 
-     //  String uri =  URLEncoder.encode(url);
+    // Here facebook has created an api and we are consuming it , so we are api client
 
-        RestTemplate restTemplate = new RestTemplate();  // this is used to call external API
-        String result = restTemplate.getForObject(uri, String.class);
+    private void callFacebookAPI() {
+        String accessToken = "EAA4uBIZCLHrEBOwcED5ZBlfReZAOHZBmHCb76h41AMxi3y6tc4Mk7ndzJA3ZC9rAh8BZAmwKWZCAbH8D1unyo9pUfCtgYjz4lTIGGn6YlqRfdGyn3pn01zwZAE6kx8LFZCmJhwUIsEZCB4Be2y8wlDegbEUASz9ucvnk68MNmt1zK020TrpdNf6ZAhwVh7S63ZBZCUryDuaz6T6Fb9iPVmoib1I4B4XfoaUYkJSFn";
+        String uri = "https://graph.facebook.com/v19.0/me/posts?fields=comments&access_token=" + accessToken;
+
+        RestTemplate restTemplate = new RestTemplate();  // this is used to call external API  in Java
+        String result = restTemplate.getForObject(uri, String.class);  // This result in json format
         System.out.println(result);
 
 
-   // Write parser and put all posts in a list and save in db
+      //  Next we need to use json parser
+        // Write parser and put all posts in a list and save in db
 
+/**
+        {
+            "data": [
+            {
+                "comments": {
+                "data": [
+                {
+                    "created_time": "2024-05-14T07:20:32+0000",
+                        "from": {
+                    "name": "AI Page",
+                            "id": "331878276672800"
+                },
+                    "message": "This is test post superr",
+                        "id": "122098716890322306_25803638655901140"
+                },
+                {
+                    "created_time": "2024-05-14T07:20:24+0000",
+                        "from": {
+                    "name": "AI Page",
+                            "id": "331878276672800"
+                },
+                    "message": "This is  nice",
+                        "id": "122098716890322306_391857713847303"
+                },
+                {
+                    "created_time": "2024-05-16T05:54:48+0000",
+                        "from": {
+                    "name": "AI Verse",
+                            "id": "7959247167458080"
+                },
+                    "message": "16th May another good post",
+                        "id": "122098716890322306_1955450111520011"
+                },
+                {
+                    "created_time": "2024-05-16T05:46:34+0000",
+                        "from": {
+                    "name": "AI Verse",
+                            "id": "7959247167458080"
+                },
+                    "message": "Good company",
+                        "id": "122098716890322306_351382277555829"
+                },
+                {
+                    "created_time": "2024-05-15T05:26:35+0000",
+                        "from": {
+                    "name": "AI Verse",
+                            "id": "7959247167458080"
+                },
+                    "message": "\ud83d\udc4d",
+                        "id": "122098716890322306_963196278686318"
+                },
+                {
+                    "created_time": "2024-05-15T06:14:32+0000",
+                        "from": {
+                    "name": "AI Verse",
+                            "id": "7959247167458080"
+                },
+                    "message": "Reply to post",
+                        "id": "122098716890322306_3278527582442955"
+                }
+        ],
+                "paging": {
+                    "cursors": {
+                        "before": "NgZDZD",
+                                "after": "MQZDZD"
+                    }
+                }
+            },
+                "id": "331878276672800_122098716890322306"
+            }
+  ],
+            "paging": {
+            "cursors": {
+                "before": "QVFIUmlVdFVRZAEtsdHhxSFRYd2ptSE4zenFZARXA3RUdFZAlhLS3N4SDNzc1M2cTdKamY0RE1lbjhUemtzdVVJNS1xSk8zdkxZAZADM1UEFtR3BtekllUnRQOG92Nmp3WTZAvTlZAIM1BDVVlKX0JjYWtuWm1sekRxODVlSm1uaHZAlSDYydGlwSDJiRDdyYndRMS0wMThaUGxzT3JWVkx6cWp4ZAk1LdkFrWTZAGVnFkRHI0TmV3SWhRck4yWDBYdTE0bzU1aDI1MGRZAUkNvVHhKbVJUa1Fmd2d2T0wxUmRQRkJhY2Y4ankxYjlrN1pOa01rRWlETXBNbndvWTBxMURQMXUwTHc1cC0weDYxZA2JQZAFVCN1ZA5SFd5QXAyZAXVZAMkE2bTNHSjgyTUs4TEwzM1RTLXhLLXNCRG12djdCWWh4QnN1N0FsZA1BJOVBBNjlvemVWcmlnejN1Um1hUDJMOFlWOGRVdC1DQ2FXc2lucThUOEZAFbjJ2Mk9WLUxDVjJnbEliLW1Dd3JsZAnB2WTFSWXdrNUZAMcFJpYjFha0dHSUoxandTTmpsX0pXcW03X1BBNkt2d28ZD",
+                        "after": "QVFIUi1OcTZAYWThtZAXg3NWduZADQ3NzJwNlk0bUJwNEZABSGpnMGptbHFsOGJOdjRkS3JQZAmFySzZADMTR2dlFoR3ZAJemZACTWJZAZAlpMVElnenpZAUHlJMjBKdjJULVN4TXBaSFFib2Jkb2lRS2lRb1AwV1R4Q2k2aU01dGdfMTJIVmVGRDk1YW1YbjN3VTQ1SHFVUWZAKYWQzZAXFlLWRCUDV2X3I3bFBVMVltSXdUZATNNMTlmb2g4aE84ZAVZATMTY2S1NCbkVYT0ZAodWw2dHFuMFl3Ym9fTXk5cWtPaDNqcFVud1RoMVUtdzBteVdUSHVLQUtYWWFUc1lOSUVLWlRVbXhUbF9HYUlIWHR6Rlc2Q1JxbjJMMUF6R1ZAyNy1IQXMzSGdPajFBSmFZAc1JSOFBOTFVWeFdvYkJqejdEYWx0N09zeWlQdFlxbjlYYXNvdk9lQVk0QWxvdUNUU293QVdvcFZAMdnFFVmNrMHJEVVFEZAUhybU9DM3NEci10LTdEN3hoaEJwMnFHOUhaQk9rYnRaek9KYzd1NmZA0NGFHNVBXRXZA3Yk9EVW1lRkpxcXdFSGpNSjQZD"
+            }
+        }
+        }
 
-
-
-      //  {"data":[{"comments":{"data":[{"created_time":"2024-05-14T07:20:32+0000","from":{"name":"AI Page","id":"331878276672800"},"message":"This is test post superr","id":"122098716890322306_25803638655901140"},{"created_time":"2024-05-14T07:20:24+0000","from":{"name":"AI Page","id":"331878276672800"},"message":"This is  nice","id":"122098716890322306_391857713847303"},{"created_time":"2024-05-16T05:54:48+0000","from":{"name":"AI Verse","id":"7959247167458080"},"message":"16th May another good post","id":"122098716890322306_1955450111520011"},{"created_time":"2024-05-16T05:46:34+0000","from":{"name":"AI Verse","id":"7959247167458080"},"message":"Good company","id":"122098716890322306_351382277555829"},{"created_time":"2024-05-15T05:26:35+0000","from":{"name":"AI Verse","id":"7959247167458080"},"message":"\ud83d\udc4d","id":"122098716890322306_963196278686318"},{"created_time":"2024-05-15T06:14:32+0000","from":{"name":"AI Verse","id":"7959247167458080"},"message":"Reply to post","id":"122098716890322306_3278527582442955"}],"paging":{"cursors":{"before":"NgZDZD","after":"MQZDZD"}}},"id":"331878276672800_122098716890322306"}],"paging":{"cursors":{"before":"QVFIUkRlczVaSzlsd0JWeVpXWTlGVXFlaE9KR1kxNDd5R2hwd3VsVmxGSnlBVXpXd0tpY0s4ZADF4SjNtX1NBTWtFS0lPVkRrR2hKU1pqYWVFMTBYMDY0STFMb3M4aTV5NHdmU0ZA2NjBwRldxMlcwMF9SWkVkMk9NNXRNTXVBT29DaWRSUFZAMcWN4VFd4cjVpTEJpeVQzZAnhURTRCZAG5qem4zYWVhZAWxUYmJaaEUxSnAwSFNaTF8ybFlBMGhCTEtqZA1hZAdHZAPRU5rcHZAmRHlGVmpGd0lCekpqN09ab0I2ZATdNczJhYjdlSTJlU2diVEZAVYWxwRVRiRlM3QXN0dlNRS0FtNmxoYTJEaWJPVENPODNwTXBXZADFfaUJ2dExDRVRULWpxdWRFaFRYbGQ0ZAjZATU2FqNEpXTjRRRlBhS3dxRTZA6M3U3SDNoMHVuc19sdzNIbWpGNFNtTGYzd3pWQzV3YkF2X3RtVHhELS1yMmpCYWM3dEd1RkU1cEVpa2ozY2RrN3AzMTVEVV8yUkl3MFlteEt3dFI3azlnLTNUczREQjZADOFZAPRnNCT0FhWWp2elkZD","after":"QVFIUmJfTW4yb1hUSUdDd0ZA4MGs2YUJGMU9JenVHVGlYWlotYVNwbzNGOWdtZA2xOa0FvVkd6RjFUWl85NFNPYmZArS191OXRldU01QU4zcG1QdHhOcnY1UnJUa2VfeGlGVk9vYXZA5ZATJsX2hoZATcwdzZAXd2pwUDQwb3lMQ0xlUXM1OGdtREpxeG1CSktVa1RQX2oweVh6dzZAQTDBKZAHJ4SWFhal9TTVVIdlM3bThHbkE4bmdEMWFQZA3ZALcVktbjBuOTdiNmY0MEZAGMnF2LURySC1lOU8zRzR6N2ZAPVGNPdTluTmZAiOVNkNFE5WWx4dkhwdld3ejBiUk5WeFJLcFFjUExxRWRjRjFUeVk3WXN5U1FjYk5YZAHFsWXJEZA1VTbjlIeF9GbWlmRE5mODU2QVVzYW9lVHZAYMlc3NnQ0WTllY1ZAUSUN3elZAJLWVTQUVhQjE3U1hxQ3ZAZAdXFpX2poU28xbXpxTmhCbno4NHJjZATRBaUdXZAEtIWWlrVnBsUUc5dV9UNl90aWJGZAExkdkxkMmp6bTNWemlFM2NHSWJSUkxPQ0E3OXlUWk1KcUpSdGZAzQkkZD"}}}
-
-
-
+        */
 
     }
 
--- src/main/java/ai/verse/scheduler/Sched.java
+++ src/main/java/ai/verse/scheduler/Sched.java
@@ -29,30 +29,30 @@ public class Sched {
     public void testData() {
         System.out.println(" --------------- testData Called on:" + new Date());
         List data = aadhaarRepository.findAll();
-        System.out.println(data);
+       System.out.println(data);
 
     }
 
 
     //  @Scheduled(fixedRate = 10000)
     public void scheduledMethod() {
-        System.out.println(" --------------- scheduledMethod Called on:" + new Date());
+      //  System.out.println(" --------------- scheduledMethod Called on:" + new Date());
 
         // List<PostEntity> list = postRepository.findAll();  // Get all posts from database
 
         List<PostEntity> list = postRepository.findRowsWithNoSentiment();  // Lists posts with no sentiment
 
-        System.out.println("--- NUMBER OF ROWS WITH NO SENTIMENT IS:  --->>" + list.size());
+    //    System.out.println("--- NUMBER OF ROWS WITH NO SENTIMENT IS:  --->>" + list.size());
 
-        // Here we are iterating over all the posts and calling sentimen class
-        for (int h = 0; h < list.size(); h++) {
-            Sentiment sm = new Sentiment();
-            PostEntity post = list.get(h);   // This will get post in a loop
-            String sentm = sm.getSentimentOfText(post.getPost());
-            post.setSentiment(sentm);
-            postRepository.save(post);  // save in database
-            System.out.println(post);
-        }
+//        // Here we are iterating over all the posts and calling sentimen class
+//        for (int h = 0; h < list.size(); h++) {
+//            Sentiment sm = new Sentiment();
+//            PostEntity post = list.get(h);   // This will get post in a loop
+//            String sentm = sm.getSentimentOfText(post.getPost());
+//            post.setSentiment(sentm);
+//            postRepository.save(post);  // save in database
+//        //    System.out.println(post);
+//        }
     }
 
 
