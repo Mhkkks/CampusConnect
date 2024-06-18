3acf3ff27ade6da9c8ebac5f1219561776abc653
Ajay <Ajay>
2024-06-18 06:46:37 +0100

    changes

--- src/main/java/ai/verse/AQIClient.java
+++ src/main/java/ai/verse/AQIClient.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
-
-import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -14,23 +12,17 @@ import org.springframework.web.client.RestTemplate;
 
 import java.io.FileOutputStream;
 import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.net.URLEncoder;
 import java.time.LocalDate;
 import java.util.Base64;
 
-public class ApiClient {
+public class AQIClient {
 
     ObjectMapper mapper = new ObjectMapper();
     public static void main(String args[]) {
-
-        ApiClient api = new ApiClient();
-
+        AQIClient api = new AQIClient();
         try {
-          //   api.callFacebookAPI();
-          //  api.callWeatherAPI();
             api.newAPI();
-         //   api.getPM25Data();
+
         } catch (Exception any) {
             any.printStackTrace();
         }
@@ -83,85 +75,6 @@ public class ApiClient {
             currentDate = currentDate.plusDays(1);
 
 
-        }
-    }
-
-    public void callWeatherAPI() throws Exception {
-
-        // 3 types
-        //1. Text example name="abc"
-        // 2. jsonNode which means an object
-        //3 ArrayNode which is an array
-
-        String url = "http://api.weatherapi.com/v1/current.json?key=405f63f293a140f69cf65216243105&q=Mumbai&aqi=yes";
-        RestTemplate restTemplate = new RestTemplate();
-        String result = restTemplate.getForObject(url, String.class);
-        System.out.println(result);
-
-        //  Step 1.    Convert String to Java JsonNode
-        JsonNode actualObj = mapper.readTree(result);
-
-        // Step 2 . Get value from jsonNode
-        // I have reeceived json string which has weather and geo details. I need latitude
-        JsonNode obj2 = actualObj.get("location");
-        String latitude = obj2.get("lat").asText();
-        System.out.println("latitude is:" + latitude);
-
-        ////////////////////////////////////////////////////////////////////
-        //   Get current -->> air_quality -->> pm10 Value
-        JsonNode currentobj = actualObj.get("current");
-        JsonNode airQuality = currentobj.get("air_quality");
-        String pm10 = airQuality.get("pm10").asText();
-        System.out.println("pm10 is:" + pm10);
-
-
-        /// Example of parsing of  array///////////////
-        String jsonArrayExample = "{\"name\":\"John\",\"age\":30,\"cars\":[\"Ford\",\"BMW\",\"Fiat\"]}";
-        JsonNode arrayExample = mapper.readTree(jsonArrayExample);
-        String name = arrayExample.get("name").asText();
-
-        // This is the way you get each element of an array
-        ArrayNode carsNode = (ArrayNode) arrayExample.get("cars"); // special for array
-        for (int g = 0; g < carsNode.size(); g++) {
-            System.out.println(carsNode.get(g));
-        }
-
-    }
-
-
-
-    // Using an api : api client
-    // Here facebook has created an api and we are consuming it , so we are api client
-    private void callFacebookAPI() {
-
-        try {
-            String accessToken = "EAA4uBIZCLHrEBO5GR1J320nZAi35erJndJdBz6TXx3joyjGipaszk1ZCBNZCIrAGTZBwJ8zmUZCoFdVeBEOlPD0br4AnZAz9TT8wpc3i21Y3ZBhNlORcVMVwdeKTRWUrRqRjZAaZBifkCtCZB7xOsMXmSIkC9ayQ3X3lWrZByDSq3H5B2J5ObVNTOzRAKKHVWjw7FwkEUrxCGPIP";
-            String uri = "https://graph.facebook.com/v19.0/me/posts?fields=comments&access_token=" + accessToken;
-
-            RestTemplate restTemplate = new RestTemplate();  // this class is used to call external API  in Java
-            String fbData = restTemplate.getForObject(uri, String.class);  // This result in json format
-            System.out.println(fbData);
-
-            JsonNode fbDataJsonNode = mapper.readTree(fbData);
-            ArrayNode fbDataNode = (ArrayNode) fbDataJsonNode.get("data");
-
-            ArrayNode fbInnerDataNode = (ArrayNode) fbDataNode.get(0).get("comments").get("data");
-
-
-            for (int g = 0; g < fbInnerDataNode.size(); g++) {
-                System.out.println(fbInnerDataNode.get(g).get("message").asText());
-            }
-
-            /**
-             1. ArrayNode -  get("data")
-             2. get zero element
-             3. get Comments
-             4. get "data"  has Array
-             5.   Each data has message
-             */
-        }catch(Exception any)
-        {
-            any.printStackTrace();
         }
     }
 
--- src/main/java/ai/verse/scheduler/AQISched.java
+++ src/main/java/ai/verse/scheduler/AQISched.java
@@ -30,11 +30,104 @@ public class AQISched {
     @Autowired
     AQIRepository aqiRepository;
 
+
+   // @Scheduled(fixedRate = 1000000)
+public void fetchAQIDataAndSave()
+{
+
+    //if(1<2) return;
+    String uri = "https://airquality.cpcb.gov.in/aqi_dashboard/aqi_all_Parameters";
+    RestTemplate restTemplate = new RestTemplate();
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Host","https://airquality.cpcb.gov.in" );
+    headers.add("accessToken","eyJ0aW1lIjoxNzE4NjA2NDAyMDM5LCJ0aW1lWm9uZU9mZnNldCI6LTYwfQ==");
+    headers.add("Cookie","_xsrf=462b9dbb4c0c44cb85a9a5b3aba67280; ccr_captcha=\"8rDT/kRz07wYMrOH2SbX6ndC5NG2ylrmlQgs3KEw7P20lrO3DPUDQhNPA4cJ6Khe20gzeTS/IZ2oYAoOVb/NYq21aOcNRZXfG9tNUVBSs6xsvAU2mgH/c3PjPWuCJn0tj4FDYJs5+fvlN9U9YxL9iF3pdD+qJi8vkwqoKKgSGhDlc921kp44uIdclW5PVQ1L7VBM0nu/RUTKPUdajOJlVZTNinPfL6Mc4Ix2TVdVZm0=\"; ccr_public=\"vKWEKbgClpRIM15O6MWHLPEsWuWTJdY2BHfTJRxUnk1VHJICC2raDAG1KyeMN0EhbiGYhMcf1Sf+Qios/nsRMbJ9Dr1XGHq1oSbYvhqYVmKL1DVpTxnQteZCtLP5BPYsRHAtfz7QpClXKqFRo80ovqjM1SDrLRkq4g/i4MqXwjP27+7QowhtNmqdYa1p2abN87wCIlAsc//Rj03RVMqZcdynaGzOiVu/+qqNQL3u+AgdxNluFYs2jfkXFaizM/fpfsTKpWU4/zJJwguPdCxf3/L/octGLrmVoBS6mylLCAcX8gbVxhZChUuSs/+jN/PR\"");
+ //   LocalDate startDate = LocalDate.of(2019, 1, 1);
+    LocalDate startDate = LocalDate.of(2013, 03, 01);
+    // End date: December 31, 2019
+    LocalDate endDate = LocalDate.of(2013, 12, 31);
+
+    // Iterate through all dates in 2019
+    LocalDate currentDate = startDate;
+
+    int i=0;
+    while (!currentDate.isAfter(endDate)) {
+
+     //   Thread.sleep(1000);
+        System.out.println(currentDate+"T00:00:00Z");
+       i++;
+
+       if(i%20==0)
+       {
+           System.out.println("Sleep 10 sec");
+           try {
+               Thread.sleep(10000);
+           } catch (InterruptedException e) {
+               throw new RuntimeException(e);
+           }
+       }
+
+        String tzDate = currentDate+"T00:00:00Z";
+        //  2019-09-21
+     //   String requestDate = "{\"station_id\":\"site_107\",\"date\":\"$DT$\"}";
+        String requestDate = "{\"station_id\":\"site_115\",\"date\":\"$DT$\"}";
+        requestDate = requestDate.replace("$DT$",tzDate);
+        System.out.println(requestDate);
+
+        String requestDateEncoded = encodeString(requestDate)+"==";
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> entity = new HttpEntity<>(requestDateEncoded, headers);
+        String response =    restTemplate.exchange(uri, HttpMethod.POST, entity, String.class).getBody();
+
+        System.out.println("Raw Response:"+response);
+
+        String responseData = decodeString(response);
+        System.out.println(responseData);
+
+        saveAQIData(currentDate.toString(), "site_115",responseData);
+
+        currentDate = currentDate.plusDays(1);
+
+    }
+}
+
+
+    public void saveAQIData(String dttime, String station, String jsonData )
+    {
+        AQIEntity aqi = new AQIEntity( );
+        aqi.setDatetime(dttime);
+        aqi.setJsondata(jsonData);
+        aqi.setStation(station);
+        aqiRepository.save(aqi);
+    }
+
+
+
+
+    public String encodeString(String decodedString)
+    {
+        String encodedString =
+                Base64.getEncoder().withoutPadding().encodeToString(decodedString.getBytes());
+        System.out.println(encodedString);
+        return encodedString;
+    }
+
+
+
+    public String decodeString(String encodedString)
+    {
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedString);
+        String decodedString = new String(decodedBytes);
+        System.out.println(decodedString);
+        return decodedString;
+    }
+
+
     // Call this automatically every 10 seconds
-//@Scheduled(fixedRate = 1000000)
+// @Scheduled(fixedRate = 1000000)
     public void updateDataAQI() {
         System.out.println(" --------------- testData Called on:" + new Date());
-        List<AQIEntity> data = aqiRepository.findAll();
+        List<AQIEntity> data = aqiRepository.findAllByIdGreaterThan(Long.valueOf(395));
         System.out.println(data);
 
         for(int k=0; k< data.size(); k++)
